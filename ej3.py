b4513a2027e591cd6908d88edbe316acdc66c7f2
ireneangulo <113629834+ireneangulo@users.noreply.github.com>
2022-12-02 11:37:43 +0000

    ej3

---
+++ ej3.py
@@ -0,0 +1,15 @@
+#desarrollar un algoritmo numerico iterativo que permita calcular el metodo de la biseccion f(x)
+import numpy as np
+def f(x):
+    return x**2-2
+def biseccion(a,b):     
+    if f(a)*f(b)>0:
+        print("no hay raiz en el intervalo")
+    else:
+        while abs(b-a)>0.0001:
+            c=(a+b)/2
+            if f(a)*f(c)<0:
+                b=c
+            else:
+                a=c
+        print("la raiz es",c)
