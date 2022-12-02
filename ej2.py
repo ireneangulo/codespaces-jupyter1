7d8733d8bea27b9c141c4c504319c8f0d6c7be76
ireneangulo <113629834+ireneangulo@users.noreply.github.com>
2022-12-02 09:57:39 +0000

    hecho

---
+++ ej2.py
@@ -0,0 +1,9 @@
+#desarrollar un algoritmo que permita pedidos de acuerdo con los niveles de prioridad de los clientes
+# 1. pedir el nombre del cliente
+# 2. pedir el nivel de prioridad del cliente
+# 3. pedir el pedido del cliente
+# 4. pedir el precio del pedido
+# 5. pedir el nombre de los productos del pedido
+
+
+
